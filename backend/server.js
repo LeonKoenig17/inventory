c4e636a5fc63f39b6d030fb9d7abe76604c3df53
Beslim17 <b.montiari@gmail.com>
2026-03-08 16:23:58 +0100

    render test

--- backend/server.js
+++ backend/server.js
@@ -65,16 +65,30 @@ app.post("/boxes", async (req, res) => {
 
 app.get("/box_inventory", async (req, res) => {
   try {
-    const { boxId } = req.query.box_id;
+    const boxId = req.query.box_id;
+
+    if (!boxId) {
+      return res.status(400).json({ error: "box_id required" });
+    }
+
     const { data, error } = await supabase
-    .from("box_inventory")
-    .select("boxes(name), items(name), quantity")
-    .eq("box_id", boxId )
-    if (error) throw error;
+      .from("box_inventory")
+      .select(`
+        quantity,
+        items ( name )
+      `)
+      .eq("box_id", boxId);
+
+    if (error) {
+      console.error("Supabase error:", error);
+      return res.status(500).json(error);
+    }
+
     res.json(data);
+
   } catch (err) {
-    console.error("GET /inventory failed:", err.message);
-    res.status(500).json({ error: err.message });
+    console.error("Server error:", err);
+    res.status(500).json({ error: "Server error" });
   }
 });
 
